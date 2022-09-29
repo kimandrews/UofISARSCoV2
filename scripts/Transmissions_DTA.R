ffe854343b925df8b7780e38f98178a2b919876d
Kim Andrews <kimandrews@gmail.com>
2022-09-29 14:04:20 -0700

    Add files via upload

--- scripts/Transmissions_DTA.R
+++ scripts/Transmissions_DTA.R
@@ -1,1 +1,40 @@
+### Author: Kim Andrews
+### Purpose: Parse output from TreeTime Discrete Trait Analysis to identify between-population transmission events
+
+# ============================= Packages ==========================
+
+require(tidyverse)
+require(ape)
+
+# ============================= Parse the TreeTime Discrete Trait Analysis ==========================
+
+# Load the tree and node data from TreeTime Discrete Trait Analysis performed using the Nextstrain ncov pipeline
+tree <- read.tree("tree.nwk")
+conf <- read_tsv("NodeConfidence.tsv") 
+dates <- read_tsv("TreeNodeDates.tsv")
+node_data <- left_join(conf, dates)
+
+# Tip names and numbers
+tips <- tree$tip.label %>% as_tibble() %>% mutate(num = 1:nrow(.)) %>% rename(name = value) 
+
+# Internal node names and numbers
+nodes <- tree$node.label %>% as_tibble() %>% mutate(num = 1:nrow(.) + nrow(tips)) %>% rename(name = value) 
+
+# Combine tips and internal nodes 
+node_numbers <- bind_rows(tips, nodes)
+
+# Branches & associated node data
+branches <- tree$edge %>% as_tibble() %>% rename(parent_num = V1, child_num = V2)
+
+branch_info <- branches %>% left_join(node_numbers, by= c("parent_num" = "num")) %>% rename(parent_name = name) %>% 
+  left_join(node_numbers, by= c("child_num" = "num")) %>% rename(child_name = name) %>% 
+  left_join(node_data, by = c("parent_name" = "node")) %>% 
+  rename(parent_population = population, parent_confidence = confidence, parent_date = date) %>% 
+  left_join(node_data, by = c("child_name" = "node")) %>% 
+  rename(child_population = population, child_confidence = confidence, child_date = date) %>% 
+  unite("pops", c("parent_population", "child_population")) 
+
+
+## Filter parent & child nodes with confidence values >= 0.8
+branch_info_conf80 <- branch_info %>% filter(parent_confidence >= 0.8) %>% filter(child_confidence >= 0.8)
 
